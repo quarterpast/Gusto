3bc9c77b3f30ac7242858086b9adc64782a5eb83
Matt Brennan <mattyb1000@gmail.com>
2011-09-06 12:51:28 +0100

    workaround bug 323890

--- template.js
+++ template.js
@@ -7,3 +7,11 @@ exports.if = function _if (cond, h, _else) {
 	}
 	return ''; // Empty string allows conditions in attribute as well as element content
 }
+exports.node = function(name,attr,cont) {
+	var out = <><{name}/></>;
+	for(let [k,v] in Iterator(attr)) {
+		out.@[k] = v;
+	}
+	out[name][0] = cont;
+	return out[name];
+}
