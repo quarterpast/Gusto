4bd0ecbd3ab37bcf236a08fc729cdc69cdb0a161
Matt Brennan <mattyb1000@gmail.com>
2011-11-22 17:30:00 +0000

    initify template.js

--- template.js
+++ template.js
@@ -1,52 +1,52 @@
 importPackage(java.io);
-
-exports.route = function(action,method) {
-	var mvc = require("mvc.js"),
-	    router = require("router.js"),
-	    routes = require(environment['user.dir']+"/conf/routes.js").routes.call(router,mvc.init().controllers()),
-	    id = Object.isFunction(action) ? action.id : action,
-	    $continue = "£$%continue, motherfucker";
-	for each(let route in routes) {
-		if(!Object.isglobal(method) && route[0] != "*" && method != route[0]) continue;
-		if(router.staticFile === route[2]) {
-			if(route[3] !== id) continue;
-			if(!(new File(id)).exists()) continue;
-			return route[1];
-		} else if(router.staticDir === route[2]) {
-			if(new File(id).getParent() != route[3]) continue;
-			return route[1].replace('{file}',new File(id).getName())
-		} else if(mvc.isAction(route[2])) {
-			if(route[2].id !== id) continue;
-			return route[1];
-			
-		} else {
-			let keys = [],
-			    out = route[2].toSource().replace(/\(function \((_?)\) \$\.?([\s\S]+);\)/,function(m,under,body) {
-				if(under === '_') {
-					let uri = route[1],
-					    reg = new RegExp("^"+body.replace(/\]\[/g,'£').replace(/^\[|\]$/g,'').replace(/_\.([\w]+)(£?)/g,function(m,key,dot){
-						keys.push(key)
-						return "([\\w0-9\.]+)"+(dot === '£' ? '\\.' : '');
-					})+"$");
-					if(!reg.test(id))
-						return $continue;
-					id.replace(reg,function(m){
-						Array.slice(arguments,1,keys.length+1).forEach(function(v,k){
-							uri = uri.replace('{'+keys[k]+'}',v);
-						})
-					});
-					return uri;
-				} else if(body === id) {
+exports.init = function(mvc,router,routes) {
+	return {
+		route: function(action,method) {
+			var id = Object.isFunction(action) ? action.id : action,
+			    $continue = "£$%continue, motherfucker";
+			for each(let route in routes) {
+				if(!Object.isglobal(method) && route[0] != "*" && method != route[0]) continue;
+				if(router.staticFile === route[2]) {
+					if(route[3] !== id) continue;
+					if(!(new File(id)).exists()) continue;
 					return route[1];
+				} else if(router.staticDir === route[2]) {
+					if(new File(id).getParent() != route[3]) continue;
+					return route[1].replace('{file}',new File(id).getName())
+				} else if(mvc.isAction(route[2])) {
+					if(route[2].id !== id) continue;
+					return route[1];
+					
+				} else {
+					let keys = [],
+					    out = route[2].toSource().replace(/\(function \((_?)\) \$\.?([\s\S]+);\)/,function(m,under,body) {
+						if(under === '_') {
+							let uri = route[1],
+							    reg = new RegExp("^"+body.replace(/\]\[/g,'£').replace(/^\[|\]$/g,'').replace(/_\.([\w]+)(£?)/g,function(m,key,dot){
+								keys.push(key)
+								return "([\\w0-9\.]+)"+(dot === '£' ? '\\.' : '');
+							})+"$");
+							if(!reg.test(id))
+								return $continue;
+							id.replace(reg,function(m){
+								Array.slice(arguments,1,keys.length+1).forEach(function(v,k){
+									uri = uri.replace('{'+keys[k]+'}',v);
+								})
+							});
+							return uri;
+						} else if(body === id) {
+							return route[1];
+						}
+					});
+					if(out === $continue || out === "undefined") continue;// no, those quotes *are* meant to be there
+					return out;
 				}
-			});
-			if(out === $continue || out === "undefined") continue;// no, those quotes *are* meant to be there
-			return out;
+			}
+			throw "routing error"
+		},
+		include: function(template,args) {
+			var template = require("app/views/"+template+".ejs").template;
+			return template.call(args,exports).toXMLString();
 		}
 	}
-	throw "routing error"
-};
-exports.include = function(template,args) {
-	var template = require("app/views/"+template+".ejs").template;
-	return template.call(args,exports).toXMLString();
-};
+}
