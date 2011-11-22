678dfee3e8b3e21723a7fb327cf2af26e4fb95a5
Matt Brennan <mattyb1000@gmail.com>
2011-11-22 22:57:39 +0000

    nomenclature

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ importPackage(Packages.com.sun.net.httpserver);
 importPackage(java.io);
 
 require("extend.js").extend(Object,String,Array,Boolean,JSON);
-exports.init = function(router) {
+exports.init = function(staticroutes) {
 	require.paths.push(this.appDir);
 	const config = JSON.parse(readFile(this.appDir+"/conf/app.conf")),
-	      routes = require(appDir+"/conf/routes.js").routes.call(router,mvc.controllers()),
+	      routes = require(appDir+"/conf/routes.js").routes.call(staticroutes,mvc.controllers()),
 	      addr = new java.net.InetSocketAddress(this[this.appMode].address || "localhost", this[this.appMode].port || 8000),
 	      server = HttpServer.create(addr, this[this.appMode].backlog || 10);
 
