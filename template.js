30ddec84bc1dfba035d2c643a95f584b879129fa
Matt Brennan <mattyb1000@gmail.com>
2011-11-22 16:40:56 +0000

    unused functions from template.js

--- template.js
+++ template.js
@@ -1,22 +1,5 @@
 importPackage(java.io);
-const router = require("router.js");
-exports.if = function _if (cond, h, _else) {// cheers, MDC
-	if (cond && cond != undefined) { // We need undefined condition for E4X
-		return h(cond);
-	}
-	else if (_else) {
-		return _else(cond);
-	}
-	return ''; // Empty string allows conditions in attribute as well as element content
-};
-exports.node = function(name,attr,cont) {// in Rhino 1.7R3, you can't embed XML in XML
-	var out = <><{name}/></>;
-	for(let [k,v] in Iterator(attr)) {
-		out.@[k] = v;
-	}
-	out[name][0] = cont;
-	return out[name];
-};
+
 exports.route = function(action,method) {
 	var mvc = require("mvc.js"),
 	    router = require("router.js"),
