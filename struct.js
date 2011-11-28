7b927bdb46c0422353086fdc82c9e9106eba5e17
Matt Brennan <mattyb1000@gmail.com>
2011-11-28 22:41:12 +0000

    struct.js

--- struct.js
+++ struct.js
@@ -1,9 +1,20 @@
 require("extend.js").extend(Object,String,Array,Boolean,JSON);
 
-const actions = {
-	
-},
-      appMode = arguments[0] in actions ? actions[arguments[0]]() : actions.help(),
-      appDir = environment['user.dir'],
-      base = JSON.parse(readFile(appDir+"/conf/app.conf")),
-      config = Object.extend(base,{appDir: appDir, appMode:appMode});
+var appDir = environment['user.dir'],
+base = JSON.parse(readFile(appDir+"/conf/app.conf")),
+config = Object.extend(base,{appDir: appDir}),
+actions = {
+	"run": function(args) {
+
+	}._({desc:"Run the app in testing mode"}),
+	"help": function(){
+		print("Struct framework\n");
+		for(let [k,v] in this) {
+			print(k.pad(8," ",false),v.desc);
+		}
+	}._({desc:"Show this help"})
+};
+if(arguments[0] in actions)
+	actions[Array.shift(arguments)](arguments);
+else
+	actions.help();
