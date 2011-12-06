f6bac135aae7f80aad320c5967e365afbd61a6aa
Matt Brennan <mattyb1000@gmail.com>
2011-12-06 22:03:43 +0000

    fuck off, java.

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-importPackage(Packages.com.sun.net.httpserver);
-importPackage(java.io);
-print(JSON.stringify(config));
-const router = require("router.js").router,
+const config = require.main.exports.config,
+http = require("http"),
+router = require("router.js").router,
 staticroute = require("staticroute.js"),
 list = require("mvc/list.js");
 
-exports.init = function() {
+modules.exports = function() {
 	require.paths.push(config.appDir);
 	const routes = require(config.appDir+"/conf/routes.js").routes.call(staticroute,list.controllers()),
 	      addr = new java.net.InetSocketAddress(config[config.appMode].address || "localhost", config[config.appMode].port || 8000),
