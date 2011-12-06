fc83c3aa5648022d55cdaab719214cbd73aacf42
Matt Brennan <mattyb1000@gmail.com>
2011-12-06 21:39:12 +0000

    nodeify struct.js

--- struct.js
+++ struct.js
@@ -1,23 +1,28 @@
 require("sugar.js");
 Object.sugar();
+const fs = require("fs");
 
-var appDir = environment['user.dir'],
-base = JSON.parse(readFile(appDir+"/conf/app.conf"));
-config = base.merge({appDir: appDir});
-var server = require("server.js"),
-actions = {
-	"run": function(args) {
-		config.merge({appMode:"testing"});
-		server.init();
-	}.merge({desc:"Run the app in testing mode"}),
-	"help": function(){
-		print("Struct framework\n");
-		for(let [k,v] in this) {
-			print(k.padRight(" ",8-k.length),v.desc);
-		}
-	}.merge({desc:"Show this help"})
-};
-if(arguments[0] in actions)
-	actions[Array.shift(arguments)](arguments);
-else
-	actions.help();
+var appDir = process.cwd();
+fs.readFile(appDir+"/conf/app.conf","utf8", function(err,data) {
+	if(err) throw err;
+	var base = JSON.parse(data);
+	    config = base.merge({appDir: appDir});
+	exports.config = config;
+	var server = require("server.js"),
+	actions = {
+		"run": function() {
+			config.merge({appMode:"testing"});
+			server.init();
+		}.merge({desc:"Run the app in testing mode"}),
+		"help": function(){
+			print("Struct framework\n");
+			this.each(function(k,v){
+				print(k.padRight(" ",8-k.length),v.desc);
+			});
+		}.merge({desc:"Show this help"})
+	};
+	if(arguments[0] in actions)
+		actions[process.argv.shift()].apply(null,process.argv);
+	else
+		actions.help();
+});
