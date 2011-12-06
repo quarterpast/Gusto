6dde45cab1ecaca48e29c37b05c34891314975db
Matt Brennan <mattyb1000@gmail.com>
2011-12-06 22:03:07 +0000

    slicker

--- struct.js
+++ struct.js
@@ -1,28 +1,26 @@
 require("sugar.js");
 Object.sugar();
 const fs = require("fs");
-
 var appDir = process.cwd();
 fs.readFile(appDir+"/conf/app.conf","utf8", function(err,data) {
 	if(err) throw err;
 	var base = JSON.parse(data);
-	    config = base.merge({appDir: appDir});
-	exports.config = config;
-	var server = require("server.js"),
-	actions = {
-		"run": function() {
-			config.merge({appMode:"testing"});
-			server.init();
-		}.merge({desc:"Run the app in testing mode"}),
-		"help": function(){
-			print("Struct framework\n");
-			this.each(function(k,v){
-				print(k.padRight(" ",8-k.length),v.desc);
-			});
-		}.merge({desc:"Show this help"})
-	};
-	if(arguments[0] in actions)
-		actions[process.argv.shift()].apply(null,process.argv);
+	    config = exports.config = base.merge({appDir: appDir}),
+	    server = require("server.js"),
+	    actions = {
+	    	"run": function() {
+	    		config.merge({appMode:"testing"});
+	    		server();
+	    	}.merge({desc:"Run the app in testing mode"}),
+	    	"help": function(){
+	    		print("Struct framework\n");
+	    		this.each(function(k,v){
+	    			print(k.padRight(" ",8-k.length),v.desc);
+	    		});
+	    	}.merge({desc:"Show this help"})
+	    };
+	if(process.argv[0] in actions)
+		actions[process.argv.shift()].apply(actions,process.argv);
 	else
 		actions.help();
 });
