4e7a89ccc0bf93fdcbce5d64f6682cb8959c8941
Matt Brennan <mattyb1000@gmail.com>
2011-12-06 23:39:46 +0000

    makes no sense to use async fs api there

--- struct.js
+++ struct.js
@@ -1,27 +1,23 @@
 require("sugar.js");
 Object.sugar();
 const fs = require("fs"),
-      appDir = process.cwd();
-
-fs.readFile(appDir+"/conf/app.conf","utf8", function(err,data) {
-	if(err) throw err;
-	var base = JSON.parse(data);
-	    config = exports.config = base.merge({appDir: appDir}),
-	    server = require("server.js"),
-	    actions = {
-	    	"run": function() {
-	    		config.merge({appMode:"testing"});
-	    		server();
-	    	}.merge({desc:"Run the app in testing mode"}),
-	    	"help": function(){
-	    		print("Struct framework\n");
-	    		this.each(function(k,v){
-	    			print(k.padRight(" ",8-k.length),v.desc);
-	    		});
-	    	}.merge({desc:"Show this help"})
-	    };
-	if(process.argv[0] in actions)
-		actions[process.argv.shift()].apply(actions,process.argv);
-	else
-		actions.help();
-});
+      appDir = process.cwd(),
+      base = JSON.parse(fs.readFileSync(appDir+"/conf/app.conf","utf8")),
+      config = exports.config = base.merge({appDir: appDir});
+    server = require("server.js"),
+    actions = {
+    	"run": function() {
+    		config.merge({appMode:"testing"});
+    		server();
+    	}.merge({desc:"Run the app in testing mode"}),
+    	"help": function(){
+    		print("Struct framework\n");
+    		this.each(function(k,v){
+    			print(k.padRight(" ",8-k.length),v.desc);
+    		});
+    	}.merge({desc:"Show this help"})
+    };
+if(process.argv[0] in actions)
+	actions[process.argv.shift()].apply(actions,process.argv);
+else
+	actions.help();
