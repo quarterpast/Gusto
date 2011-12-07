d15789788a12fb48e7d7cad25c81485ef0710f02
Matt Brennan <mattyb1000@gmail.com>
2011-12-07 08:47:05 +0000

    should have gone to specsavers

--- struct.js
+++ struct.js
@@ -3,20 +3,20 @@ Object.sugar();
 const fs = require("fs"),
       appDir = process.cwd(),
       base = JSON.parse(fs.readFileSync(appDir+"/conf/app.conf","utf8")),
-      config = exports.config = base.merge({appDir: appDir});
-    server = require("server.js"),
-    actions = {
-    	"run": function() {
-    		config.merge({appMode:"testing"});
-    		server();
-    	}.merge({desc:"Run the app in testing mode"}),
-    	"help": function(){
-    		print("Struct framework\n");
-    		this.each(function(k,v){
-    			print(k.padRight(" ",8-k.length),v.desc);
-    		});
-    	}.merge({desc:"Show this help"})
-    };
+      config = exports.config = base.merge({appDir: appDir}),
+      server = require("server.js"),
+      actions = {
+      	"run": function() {
+      		config.merge({appMode:"testing"});
+      		server();
+      	}.merge({desc:"Run the app in testing mode"}),
+      	"help": function(){
+      		print("Struct framework\n");
+      		this.each(function(k,v){
+      			print(k.padRight(" ",8-k.length),v.desc);
+      		});
+      	}.merge({desc:"Show this help"})
+      };
 if(process.argv[0] in actions)
 	actions[process.argv.shift()].apply(actions,process.argv);
 else
