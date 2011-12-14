feef93fd7a7461596bc5cca926876ae1b6a24c5f
Matt Brennan <mattyb1000@gmail.com>
2011-12-14 10:34:31 +0000

    1.6 syntax

--- mvc/list.js
+++ mvc/list.js
@@ -1,19 +1,32 @@
-importPackage(java.io);
+const fs = require("fs"),
+      path = require("path");
+
+require.cache
 function fromFiles(folder,skip) {
-	var files = new File(folder).listFiles()
-	                .filter(function(f) f.getName().substr(-3) == ".js"),
-	    objects = {};
-	for each(let file in files) {
-		if(file.getName() === skip) continue;
-		let basename = file.getName().substring(0,file.getName().length()-3)
-		objects[basename] = require(file.getPath())[basename];
-	}
+	var objects = {},
+	files = fs.readDirSync(folder).filter(function(f) {
+		return f.endsWith(".js");
+	});
+	files.each(function(file) {
+		if(file === skip) continue;
+		var basename = file.remove(/\.js$/);
+		objects[basename] = require(path.join(folder,file));
+	});
 	return objects;
 }
 
-exports.controllers = function(id) fromFiles("app/controllers",id);
-exports.models = function(id) fromFiles("app/models",id);
-exports.isModel = function(m) this.fromFiles("app/models").indexOf(m) !== -1;
-exports.isController = function(m) this.fromFiles("app/controllers").indexOf(m) !== -1;
-exports.isAction = function(m) Object.isFunction(m) && "id" in m;
-exports.models = function(id) this.fromFiles("app/models",id);
+exports.controllers = function(id) {
+	return fromFiles("app/controllers",id);
+}
+exports.models = function(id) {
+	return fromFiles("app/models",id);
+}
+exports.isModel = function(m) {
+	return fromFiles("app/models").indexOf(m) !== -1;
+}
+exports.isController = function(m) {
+	return this.fromFiles("app/controllers").indexOf(m) !== -1;
+}
+exports.isAction = function(m) {
+	return Object.isFunction(m) && "id" in m;
+}
