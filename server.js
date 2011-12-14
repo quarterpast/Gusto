c56845f67398ae36c73ab0f444324008260345e3
Matt Brennan <mattyb1000@gmail.com>
2011-12-14 23:30:15 +0000

    don't need to be in the function

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const config = require.main.exports.config,
 http = require("http"),
 router = require("router.js"),
 staticroute = require("staticroute.js"),
-list = require("mvc/list.js");
+list = require("mvc/list.js"),
+routes = require(config.appDir+"/conf/routes.js").call(staticroute,list.controllers()),
+server = http.createServer(function(req,res) {
+	
+	//@TODO: parse headers, request body
+	routes.filter(require("router.js").bind(null,req));
+	//@TODO: write out the content
+}),
+port = config[config.appDir].port || 8000;
 
 exports.go = function() {
-	const routes = require(config.appDir+"/conf/routes.js").call(staticroute,list.controllers()),
-	      server = http.createServer(function(req,res) {
-	      	
-	      	//@TODO: parse headers, request body
-	      	routes.filter(require("router.js").bind(null,req));
-	      	//@TODO: write out the content
-	      }),
-	      port = config[config.appDir].port || 8000;
 	if("address" in config[config.appDir]) {
 		server.listen(
 			port,
