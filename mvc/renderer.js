915e8415181a158eb05acb74b3b089c45fb3b571
Matt Brennan <mattyb1000@gmail.com>
2011-12-15 13:48:35 +0000

    recursion

--- mvc/renderer.js
+++ mvc/renderer.js
@@ -1,20 +1,27 @@
-module.exports = function Renderer(path,args) {
+const extensions = require("template.js");
+module.exports = function Renderer(path,args,layout) {
 	var resolved = pathutil.join("app/views/",path+".ejs"),
+	old = path,
 	that = this;
 	fs.readFile(resolved,function(err,data) {
 		if(err) throw err;
 		var comp = tmpl.compile(data,resolved),
-		output = comp.runInNewContext({
-			$: {
+		output = comp.runInNewContext(args.merge({
+			$: extensions.merge({
 				extend: function(daddy) {path = daddy},
-				layout: output,
+				layout: layout,
 				set: function(k,v){args[k]=v;},
 				get: function(k) {return args[k]},
 				exists: function(k) {return k in args}
-			}
+			})
 		});
-
-		that.emit("render",output)
+		if(old != path) {
+			new Renderer(path,args,output).on("render",function(output) {
+				that.emit("render",output);
+			});
+		} else {
+			that.emit("render",output);
+		}
 	});
 }
 module.exports.prototype = new process.EventEmitter();
