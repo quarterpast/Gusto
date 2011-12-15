55a6fec35895cfe3901ce8bf96ff34846053f357
Matt Brennan <mattyb1000@gmail.com>
2011-12-15 11:46:26 +0000

    yay short circuit

--- mvc/controller.js
+++ mvc/controller.js
@@ -17,7 +17,7 @@ module.exports = function(actions) {
 				action = args;
 				args = other;
 			}
-			args = Object.isglobal(args) ? {} : args;
+			args = args || {};
 
 			var path = (base ? base+"/" : "")+action,
 			    oldpath = '',
