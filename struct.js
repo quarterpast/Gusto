139a4f876c62d552656c54a9d582856b8f8759c9
Matt Brennan <mattyb1000@gmail.com>
2011-12-15 14:28:26 +0000

    whitespace

--- struct.js
+++ struct.js
@@ -1,22 +1,22 @@
 require("sugar.js");
 Object.sugar();
 const fs = require("fs"),
-      appDir = process.cwd(),
-      base = JSON.parse(fs.readFileSync(appDir+"/conf/app.conf","utf8")),
-      config = exports.config = base.merge({appDir: appDir}),
-      server = require("server.js"),
-      actions = {
-      	"run": function() {
-      		config.merge({appMode:"testing"});
-      		server.go();
-      	}.merge({desc:"Run the app in testing mode"}),
-      	"help": function(){
-      		print("Struct framework\n");
-      		this.each(function(k,v){
-      			print(k.padRight(" ",8-k.length),v.desc);
-      		});
-      	}.merge({desc:"Show this help"})
-      };
+appDir = process.cwd(),
+base = JSON.parse(fs.readFileSync(appDir+"/conf/app.conf","utf8")),
+config = exports.config = base.merge({appDir: appDir}),
+server = require("server.js"),
+actions = {
+	"run": function() {
+		config.merge({appMode:"testing"});
+		server.go();
+	}.merge({desc:"Run the app in testing mode"}),
+	"help": function(){
+		print("Struct framework\n");
+		this.each(function(k,v){
+			print(k.padRight(" ",8-k.length),v.desc);
+		});
+	}.merge({desc:"Show this help"})
+};
 if(process.argv[0] in actions)
 	actions[process.argv.shift()].apply(actions,process.argv);
 else
