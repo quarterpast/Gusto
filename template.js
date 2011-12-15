fa3709b7e2cdc10dd62789d6753b39cabdb6ff5c
Matt Brennan <mattyb1000@gmail.com>
2011-12-15 20:37:54 +0000

    dejava

--- template.js
+++ template.js
@@ -1,29 +1,29 @@
-importPackage(java.io);
 const list = require("mvc/list.js"),
 static = require("static.js"),
-config = require("config.js").config,
-routes = require(config.appDir+"/conf/routes.js");
+config = require.main.exports.config,
+routes = require(config.appDir+"/conf/routes.js"),
+path = require("path.js");
 exports.route = function(action,method) {
 	var id = Object.isFunction(action) ? action.id : action,
-	    $continue = "£$%continue, motherfucker";
-	routes.filter(function(route) {
-		if(!Object.isglobal(method) && route[0] != "*" && method != route[0]) continue;
+	    $continue = "£$%continue, motherfucker",
+	filter = routes.map(function(route) {
+		if(!Object.isglobal(method) && route[0] != "*" && method != route[0]) return null;
 		if(static.file === route[2]) {
-			if(route[3] !== id) continue;
-			if(!(new File(id)).exists()) continue;
+			if(route[3] !== id) return null;
+			if(!path.existsSync(id)) return null;
 			return route[1];
 		} else if(static.dir === route[2]) {
-			if(new File(id).getParent() != route[3]) continue;
-			return route[1].replace('{file}',new File(id).getName())
+			if(path.dirname(id) != route[3]) return null;
+			return route[1].replace('{file}',path.basename(id));
 		} else if(list.isAction(route[2])) {
-			if(route[2].id !== id) continue;
+			if(route[2].id !== id) return null;
 			return route[1];
 			
 		} else {
-			let keys = [],
+			var keys = [],
 			    out = route[2].toSource().replace(/\(function \((_?)\) \$\.?([\s\S]+);\)/,function(m,under,body) {
 				if(under === '_') {
-					let uri = route[1],
+					var uri = route[1],
 					    reg = new RegExp("^"+body.replace(/\]\[/g,'£').replace(/^\[|\]$/g,'').replace(/_\.([\w]+)(£?)/g,function(m,key,dot){
 						keys.push(key)
 						return "([\\w0-9\.]+)"+(dot === '£' ? '\\.' : '');
@@ -31,7 +31,7 @@ exports.route = function(action,method) {
 					if(!reg.test(id))
 						return $continue;
 					id.replace(reg,function(m){
-						Array.slice(arguments,1,keys.length+1).forEach(function(v,k){
+						Array.slice(arguments,1,keys.length+1).each(function(v,k){
 							uri = uri.replace('{'+keys[k]+'}',v);
 						})
 					});
@@ -40,11 +40,14 @@ exports.route = function(action,method) {
 					return route[1];
 				}
 			});
-			if(out === $continue || out === "undefined") continue;// no, those quotes *are* meant to be there
+			if(out === $continue || out === "undefined") return null;// no, those quotes *are* meant to be there
 			return out;
 		}
-	});
-	throw "routing error"
+	}).compact();
+	if(filter.length) {
+		return filter[0];
+	}
+	throw new SyntaxError("Could not route "+id);
 };
 exports.include = function(template,args) {
 	var template = require("app/views/"+template+".ejs").template;
