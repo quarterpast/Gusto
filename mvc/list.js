911ae830bddca427b8af4b6de303f2c3d690f116
Matt Brennan <mattyb1000@gmail.com>
2011-12-21 23:02:54 +0000

    runs at load, don't need to be async

--- mvc/list.js
+++ mvc/list.js
@@ -2,25 +2,20 @@ const fs = require("fs"),
       path = require("path"),
       hot = require("hot");
 
-var cache = {};
 
-module.exports = function fromFiles(thing,cb) {
-	if(thing in cache) {
-		return cb(cache[thing]);
-	}
-	fs.readdir(path.join("app",thing),function(err,files) {
-		var out = {};
-		cache[thing] = out;
-		if(err) throw err;
-		files.each(function(file) {
-			function save(module) {
-				out[base] = module;
-			}
-			if(path.extname(file) == '.js') {
-				var base = path.basename(file,".js");
-				new hot.load(path.join("app",thing,file),save).on("reload",save);
-			}
-		});
-		cb(out);
+function fromFiles(thing) {
+	var out = {};
+	fs.readdirSync(path.join("app",thing)).each(function(file) {
+		var base = path.basename(file,".js");
+		function save(module) {
+			out[base] = module;
+		}
+		if(path.extname(file) == '.js') {
+			new hot.load(path.join("app",thing,file),save)
+			       .on("reload",save);
+		}
 	});
-};
+	return out;
+}
+exports.controllers = fromFiles("controllers");
+exports.models = fromFiles("models");
