275113b3e8edc7f242eb6da6a25b77f978e74b5e
Matt Brennan <mattyb1000@gmail.com>
2011-12-22 13:33:11 +0000

    make sure mode reaches the server

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 require("sugar.js");
 Object.sugar();
-exports.mode = "testing";
-exports.setMode = function(m) {
-	exports.mode = m;
-}
 const fs = require("fs"),
 appDir = process.cwd(),
 base = JSON.parse(fs.readFileSync(appDir+"/conf/app.conf","utf8")),
 config = exports.config = base.merge({appDir: appDir}),
-server = require("server.js"),
 actions = {
-	"run": function() {
-		server.go();
+	"run": function(mode) {
+		exports.mode = mode || "testing";
+		require("server.js").go();
 	}.merge({desc:"Run the app in testing mode"}),
 	"help": function(){
 		console.log("Struct framework\n");
