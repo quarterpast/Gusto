bee0a6d51de17f0210ec67397b914bfb99d20296
Matt Brennan <mattyb1000@gmail.com>
2011-12-27 16:47:53 +0000

    clean up flow, remove old code

--- template.js
+++ template.js
@@ -13,43 +13,26 @@ routes = data.split(/[\n\r]/).map(function(line) {
 path = require("path");
 exports.route = function(action,method) {
 	var id = Object.isFunction(action) ? action.id : action,
-	    $continue = "£$%continue, motherfucker",
 	filter = routes.map(function(route) {
 		if(!!method && route[0] != "*" && method != route[0]) return null;
+
 		if("static.file" === route[2]) {
 			if(route[3] !== id) return null;
 			if(!path.existsSync(id)) return null;
 			return route[1];
-		} else if("static.dir" === route[2]) {
+		}
+
+		if("static.dir" === route[2]) {
 			if(path.dirname(id) != route[3]) return null;
 			return route[1].replace('{file}',path.basename(id));
-		} else if(list.isAction(route[2])) {
+		}
+
+		if(list.isAction(route[2])) {
 			if(route[2] !== id) return null;
 			return route[1];
-		} else {
-			var keys = [],//@TODO: for new routes fmt
-			    out = route[2].replace(/\(function \((_?)\) \$\.?([\s\S]+);\)/,function(m,under,body) {
-				if(under === '_') {
-					var uri = route[1],
-					    reg = new RegExp("^"+body.replace(/\]\[/g,'£').replace(/^\[|\]$/g,'').replace(/_\.([\w]+)(£?)/g,function(m,key,dot){
-						keys.push(key)
-						return "([\\w0-9\.]+)"+(dot === '£' ? '\\.' : '');
-					})+"$");
-					if(!reg.test(id))
-						return $continue;
-					id.replace(reg,function(m){
-						Array.slice(arguments,1,keys.length+1).each(function(v,k){
-							uri = uri.replace('{'+keys[k]+'}',v);
-						})
-					});
-					return uri;
-				} else if(body === id) {
-					return route[1];
-				}
-			});
-			if(out === $continue || out === "undefined") return null;// no, those quotes *are* meant to be there
-			return out;
 		}
+
+		
 	}).compact();
 	if(filter.length) {
 		return filter[0];
