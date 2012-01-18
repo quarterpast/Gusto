f115f4b0c8f2a6d6b56b543d534b0baabbc5c518
Matt Brennan <mattyb1000@gmail.com>
2012-01-18 19:19:09 +0000

    filters now return EEs

--- static.js
+++ static.js
@@ -8,17 +8,25 @@ filters = pathutil.existsSync(fpath) ? require(fpath) : {};
 
 exports.file = function(result,path) {
 	var read = fs.createReadStream(path),
-	type = mime.lookup(path),
-	filter = {content:function(c){return c;}};
+	type = mime.lookup(path);
 
 	if(pathutil.extname(path) in filters) {
-		filter = filters[pathutil.extname(path)];
+		var filter = filters[pathutil.extname(path)];
 		type = filter.mime;
 	}
 	read.resume();
 	read.on("error", function(error) {
+		result.emit("done",404,path+" not found.");
 	}).on("data",function(chunk) {
-		result.emit("queue",["write",filter.content(chunk)]);
+		if(filter) {
+			filter.content(chunk).on("error",function(e) {
+				result.emit("done",500,"could not filter");
+			}).on("done",function(out) {
+				result.emit("queue",["write",out]);
+			});
+		} else {
+			result.emit("queue",["write",chunk]);
+		}
 	}).on("end",function() {
 		result.emit("done",200,{"Content-type":type});
 	});
