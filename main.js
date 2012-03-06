a856ca364ce691b2b5a771c693b587021e203910
Matt Brennan <mattyb1000@gmail.com>
2012-03-06 04:38:35 +0000

    port main.js

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
-require("sugar");// library with sweet API additions
-Object.extend(); // attach things to Object.prototype
-
-const cluster = require("cluster"),
-fs = require("fs"),
-path = require("path"),
-numCPUs = require('os').cpus().length,
-appDir = process.cwd(),
-pidFile = path.join(appDir,"struct.pid");
-
-exports.run = function(base) {
-	function fork() {
-		var pid = cluster.fork().pid.toString();
-		fs.open(path.join("pids.d",pid),"w");
-	}
-	var config = exports.config = base.merge({appDir: appDir});
-	
-	exports.mvc = {// so the app can use them
-		list: require("./mvc/list.js"),
-		model: require("./mvc/model.js")
-	};
-	// if the app wants us to fork and this is the master process...
-	if(config.cluster && cluster.isMaster) {
-		for (var i = 0; i < numCPUs; i++) {
-			fork();
-		}
-		cluster.on('death', function(hamster) {
-			// log the deaths of any child processes
-			console.log('hamster ' + hamster.pid + ' died');
-			fs.unlink(path.join("pids.d",hamster.pid.toString()));
-			// and, if the app wants, respawn it
-			config.respawn && fork();
-		});
-	} else {
-		// this is a child process, or we're not forking
-		// either way, init and run the server
-		require("./server/server.js").go();
-	}
-};
+(function(){
+  var cluster, fs, path, numCPUs, appDir, pidFile;
+  require('sugar');
+  Object.extend();
+  cluster = require('cluster');
+  fs = require('fs');
+  path = require('path');
+  numCPUs = require('os').cpus().length;
+  appDir = process.cwd();
+  pidFile = path.join(appDir, "struct.pid");
+  exports.run = function(base){
+    var fork, config, i, _to;
+    fork = function(){
+      var pid;
+      pid = cluster.fork().pid.toString();
+      return fs.open(path.join("pids.d", pid), "w");
+    };
+    config = exports.config = base.merge({
+      appDir: appDir
+    });
+    exports.mvc = {
+      list: require("./mvc/list.js"),
+      model: require("./mvc/model.js")
+    };
+    if (config.cluster && cluster.isMaster) {
+      for (i = 0, _to = numCPUs; i < _to; ++i) {
+        fork();
+      }
+      return cluster.on['death'](function(hamster){
+        console.log('hamster #{hamster.pid} died');
+        fs.unlink(path.join("pids.d", hamster.pid.toString()));
+        if (config.respawn) {
+          return fork();
+        }
+      });
+    } else {
+      return require("./server/server.js").go();
+    }
+  };
+}).call(this);
