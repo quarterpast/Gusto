3e6fddd314831a6e8d53757e7fa6a9b8fd119e5e
Matt Brennan <mattyb1000@gmail.com>
2012-03-06 05:08:31 +0000

    port router.js

--- server/router.js
+++ server/router.js
@@ -1,71 +1,64 @@
-// router.js
-// a filter function. takes a route, returns an action (or nothing)
-const url = require("url"),
-list = require("../mvc/list.js"),
-instance = require("../mvc/instance.js"),
-staticRoute = require("./static.js"),
-redirect = require("./redirect.js");
-
-module.exports = function Router(req,res,route) {
-	var params = {}, keys = [], uri = url.parse(req.url,true);
-	if(route[0] === "*" || route[0] == req.method) {
-		// method matches
-		var reg = new RegExp(
-			// build a regexp from the placeholders in the url
-			"^"+route[1]
-			.replace(
-				/\{([\w]+?)(\|[\s\S]+?)?(\/)?\}/g,
-				function(m,key,sub,slash){
-					keys.push(key);
-					if(sub) {// route specifies its own regexp
-						return sub.substr(1);
-					}
-					if(slash == '/') {
-						return "((/?[^/?*:;{}\\\\]+)+)";
-					}
-					return "([\\w0-9.-]+)";
-				}
-			)+
-			"$");
-
-		if(reg.test(uri.pathname)) {
-			uri.pathname.replace(reg,function(m){
-				// grab variables from the url
-				for(var i = 1, l = keys.length; i <= l; ++i) {
-					params[keys[i-1]] = arguments[i];
-				}
-			});
-			var env = ({}).merge(list.controllers)
-			              .merge(params)
-			              .merge({
-				              static: staticRoute,
-				              redirect: redirect
-				            });
-			try {
-				// evaluate the action id
-				action = route[2].runInNewContext(env);
-			} catch(e) {
-				if(e.name == "TypeError") {
-					return;// doesn't exist
-				} else throw e;
-			}
-			if(!action) return null;
-			var id = action.id, run, bits = id.split('.');
-
-			if([// give it the right context
-				"static.file",
-				"static.dir",
-				"static.url",
-				"static.template",
-				"redirect"
-			].some(id)) {
-				run = action.bind(null,req,res,route[3]);
-			} else {
-				methods = instance(res,bits[0],bits[1]);
-				run = action.bind(action.context.merge(methods));
-			}
-			run.params = params;
-			return run;
-		}
-	}
-};
+(function(){
+  var url, list, instance, staticRoute, redirect;
+  url = require('url');
+  list = require("../mvc/list.co");
+  instance = require("../mvc/instance.co");
+  staticRoute = require("./static.co");
+  redirect = require("./redirect.co");
+  module.exports = function(req, res, route){
+    var params, keys, uri, reg, env, action, id, bits, methods, run, _ref;
+    params = {};
+    keys = [];
+    uri = url.parse(req.url, true);
+    if (route[0] === "*" || route[0] === req.method) {
+      reg = new RegExp("^" + route[1].replace(/\{([\w]+?)(\|[\s\S]+?)?(\/)?\}/g, function(m, key, sub, slash){
+        keys.push(key);
+        if (sub) {
+          return sub.substr(1);
+        } else if (slash === '/') {
+          return "((/?[^/?*:;{}\\\\]+)+)";
+        } else {
+          return "([\\w0-9.-]+)";
+        }
+      }), +"$");
+      if (reg.test(uri.pathname)) {
+        uri.pathname.replace(reg, function(m){
+          var i, _to, _results = [];
+          for (i = 1, _to = keys.length; i <= _to; ++i) {
+            _results.push(params[keys[i - 1]] = arguments[i]);
+          }
+          return _results;
+        });
+        env = (_ref = __import(__clone(list.controllers), params), _ref['static'] = staticRoute, _ref.redirect = redirect, _ref);
+        try {
+          action = route[2].runInNewContext(env);
+        } catch (e) {
+          if (e.name === '"TypeError"') {
+            return;
+          } else {
+            throw e;
+          }
+        }
+        if (!action) {
+          return null;
+        }
+        id = action.id;
+        bits = id.split('.');
+        run = id === "static.file" || id === "static.dir" || id === "static.url" || id === "static.template" || id === "redirect"
+          ? action.fill(req, res, route[3])
+          : (methods = instance(res, bits[0], bits[1]), action.bind(__import(action.context, methods)));
+        run.params = params;
+        return run;
+      }
+    }
+  };
+  function __import(obj, src){
+    var own = {}.hasOwnProperty;
+    for (var key in src) if (own.call(src, key)) obj[key] = src[key];
+    return obj;
+  }
+  function __clone(it){
+    function fun(){} fun.prototype = it;
+    return new fun;
+  }
+}).call(this);
