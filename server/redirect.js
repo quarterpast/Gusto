ba6b588d3c2840287c02161309c5454f5f5ad04a
Matt Brennan <mattyb1000@gmail.com>
2012-03-08 10:33:59 +0000

    remove compiled js

--- server/redirect.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  module.exports = function(req, res, path){
-    res.writeHead(302, {
-      Location: path
-    });
-    return res.end();
-  };
-  module.exports.id = 'redirect';
-}).call(this);
